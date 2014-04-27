d905a9ff7216540bf1c118eff87f42b1c9f0b4af
Christian Forster <ch.forster@gmail.com>
2014-04-27 23:34:31 +0200

    refactor

--- svo/include/svo/feature.h
+++ svo/include/svo/feature.h
@@ -32,11 +32,11 @@ struct Feature
   };
 
   FeatureType type;     //!< Type can be corner or edgelet.
-  Frame*   frame;       //!< Pointer to frame in which the feature was detected.
+  Frame* frame;         //!< Pointer to frame in which the feature was detected.
   Vector2d px;          //!< Coordinates in pixels on pyramid level 0.
   Vector3d f;           //!< Unit-bearing vector of the feature.
-  int      level;       //!< Image pyramid level where feature was extracted.
-  Point*   point;       //!< Pointer to 3D point which corresponds to the feature.
+  int level;            //!< Image pyramid level where feature was extracted.
+  Point* point;         //!< Pointer to 3D point which corresponds to the feature.
   Vector2d grad;        //!< Dominant gradient direction for edglets, normalized.
 
   Feature(Frame* _frame, const Vector2d& _px, int _level) :
